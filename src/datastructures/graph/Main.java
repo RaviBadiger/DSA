8eea2d45acfa018bec6dc2fb28cda7811fe55846
RaviBadiger <badigar.rav18@gmail.com>
2022-10-21 16:12:22 +0530

    Added Graph

---
+++ src/datastructures/graph/Main.java
@@ -0,0 +1,21 @@
+package datastructures.graph;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph myGraph = new Graph();
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addVertex("C");
+        myGraph.addVertex("D");
+
+        myGraph.addEdge("A","B");
+        myGraph.addEdge("B","C");
+        myGraph.addEdge("A","C");
+        myGraph.addEdge("A","D");
+        myGraph.addEdge("B","D");
+        myGraph.addEdge("C","D");
+        myGraph.printGraph();
+        myGraph.removeVertex("D");
+        myGraph.printGraph();
+    }
+}
